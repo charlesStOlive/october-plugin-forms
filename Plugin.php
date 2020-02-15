cb2bfb838053f4160172b0866c31ea303465f714
charles <charles.stolive@dayonmars.fr>
2020-02-15 14:00:14 +0100

    correction case

--- Plugin.php
+++ Plugin.php
@@ -87,7 +87,7 @@ class Plugin extends PluginBase
             'Martin\Forms\Components\GenericForm' => 'genericForm',
             'Martin\Forms\Components\UploadForm' => 'uploadForm',
             'Martin\Forms\Components\EmptyForm' => 'emptyForm',
-            'Martin\Forms\Components\WakaForm' => 'wakaform',
+            'Martin\Forms\Components\WakaForm' => 'wakaForm',
         ];
     }
 
